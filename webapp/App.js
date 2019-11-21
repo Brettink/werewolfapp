08a075aa2b5cca1712e4def2382fb148ae884797
Brett <brett_wad_12@hotmail.com>
2019-11-21 00:31:30 -0600

    Added styling and the beggining of the on-top canvas container

--- webapp/App.js
+++ webapp/App.js
@@ -1,35 +1,47 @@
-useFirebase = true;
+useFirebase = true, ontopCanvas = true;
 libPath = "srclib/lib/";
 basePath = "webapp/";
 
-frontC = null, backC = null;
-divCon = null, flipB = null;
+frontC = null; backC = null;
+divCon = null; flipB = null;
+canCon = null; butCon = null;
 front = true;
 
 function init(){
 
+    self.butCon = createTag("span","but");
     self.flipB = createTag("button", "flip");
-    self.frontC = createTag("img", "frontC");
-    self.backC = createTag("img", "backC");
-    self.divCon = createTag("div", "cc");
+    self.frontC = createTag("img", "img");
+    self.backC = createTag("img", "img");
+    self.divCon = createTag("div", "dc");
+    self.canCon = createTag("div", "cc");
+    self.butCon.insertChild(self.flipB);
 
     self.frontC.src = basePath + "imgs/cc1.png";
     self.backC.src = basePath + "imgs/cc2.png";
+
+    self.frontC.setStyle("display", "block");
     self.backC.setStyle("display", "none");
-    self.divCon.setStyle("margin", "auto");
-    self.divCon.setStyle("width", "60%");
+
     self.divCon.insertChild(self.frontC);
     self.divCon.insertChild(self.backC);
     self.flipB.text = "Flip Paper";
+
     self.flipB.setMouseCallback(e=>{
         self.front = !self.front;
         self.frontC.setStyle("display", (self.front)? "block": "none");
         self.backC.setStyle("display", (!self.front)? "block": "none");
     });
     createCanvas(100, 100);
+    self.canCon.insertChild(canvas);
+    self.divCon.insertChild(canCon);
 
 }
-
+x = 0; y = 0;
 function loop(){
+    //this.divCon.tag.style['width'] = document.body.clientWidth + "px";
+    let tagGet = (this.front)?this.frontC:this.backC;
+    canvas.width = tagGet.tag.offsetWidth;
+    canvas.height = tagGet.tag.offsetHeight*.999999;
 
 }
